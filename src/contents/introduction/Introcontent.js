88a69de0c6ad57ce62700b4a68c5cf6dd5e00d4b
sayyidahakindele <akindelesayyidah@gmail.com>
2024-09-12 02:13:50 -0400

    introduction page css

--- src/contents/introduction/Introcontent.js
+++ src/contents/introduction/Introcontent.js
@@ -3,10 +3,10 @@ import self from '../photos/photo.png';
 import './Introcontent.css';
 
 const Introcontent = () => (
-    <div id="container" className="container mt-5">
+    <div className="container mt-5">
 		<div className="row">
 			<div className="col-12 col-md-9 order-md-1 order-2 text-center text-md-start">
-				<h3>Hi! My name is</h3>
+				<h3>Hi there! My name is</h3>
 				<h2>DOYINSOLA SAYYIDAH AKINDELE</h2>
 				<p>I'm a software developer studying Computer Science and Psychology at Carleton University.</p>
 				<p>Although I was born in Nigeria, I have spent half of my life living abroad — first in Oman during my childhood and 
@@ -21,11 +21,10 @@ const Introcontent = () => (
 					development, although I continue to have keen interest in web development and database management.
 				</p>
 				<p>My skill set extends to building responsive, user-centric web and mobile applications, demonstrated through 
-					multiple internships and personal projects. I am proficient in technologies like React, MongoDB, and SQL, allowing me 
-					to create scalable, dynamic applications that prioritize the user experience. 
-					Whether I’m working on high-level interfaces or optimizing low-level system performance, I am committed to delivering 
-					well-structured, functional, and scalable solutions. I am eager to collaborate on innovative projects that challenge 
-					the boundaries of software development and enhance users' interactions with technology.
+					multiple internships and personal projects. Whether I’m working on high-level interfaces or optimizing low-level 
+					system performance, I am committed to delivering well-structured, functional, and scalable solutions. I am eager 
+					to collaborate on innovative projects that challenge the boundaries of software development and enhance users' 
+					interactions with technology.
 				</p>
 				<p>I've loved embroidering, cooking and baking since I was a kid and I recently taught myself to crochet. Knitting and sewing are next on my list! I also enjoy reading and writing which I try to do outside when the weather is warm.</p>
 			</div>
